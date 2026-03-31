778179f4f55699cd5cc580c1578b8d0c0cb6bcc4
Saksham gupta <163002102+capable-coder@users.noreply.github.com>
2026-03-31 19:35:50 +0530

    Update config.py

--- pokemonster/config.py
+++ pokemonster/config.py
@@ -4,15 +4,25 @@ import os
 
 load_dotenv()
 
-devs = [6149191605, 5146000168, 1344569458, 2065219675, 861432102, 1432756163, 6279469935]
+devs = [8400280060]
 users_filt = filters.user(devs)
 
-api_id = int(os.getenv("API_ID"))
-hash = os.getenv("API_HASH")
-token = os.getenv("BOT_TOKEN")
+# ---------------- SAFE ENV LOADER ----------------
+def get_env(*keys, default=None):
+    for key in keys:
+        val = os.getenv(key)
+        if val:
+            return val
+    return default
 
-MONGO_URI = os.getenv("MONGO_URI_1")
-MONGO_NAME = os.getenv("MONGO_NAME_1")
 
-MONGO_URI2 = os.getenv("MONGO_URI_2")
-MONGO_NAME2 = os.getenv("MONGO_NAME_2")
+API_ID = int(get_env("API_ID"))
+API_HASH = get_env("API_HASH")
+BOT_TOKEN = get_env("BOT_TOKEN")
+
+# ---------------- MONGO (UNIFIED NAMES) ----------------
+MONGO_URI = get_env("MONGO_URI_1", "MONGO_URI", "MONGO_URL")
+MONGO_NAME = get_env("MONGO_NAME_1", "MONGO_NAME", default="pocketmonsters")
+
+MONGO_URI2 = get_env("MONGO_URI_2")
+MONGO_NAME2 = get_env("MONGO_NAME_2", default="pocketmonsters")
